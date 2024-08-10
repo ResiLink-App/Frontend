f143815329546fc18739fc5dcf58b9869efbd44e
David Horjet <davidojetola956@gmail.com>
2024-08-10 15:34:49 +0100

    chore: code update

--- src/utils/apiRoutes.tsx
+++ src/utils/apiRoutes.tsx
@@ -1,6 +1,6 @@
 import { domain } from "./helpers";
 
-export const BASE_URL = "http://localhost:8080";
-// export const BASE_URL = "https://fusion-api-z53r.onrender.com";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://resilink-backend.onrender.com";
 
 export const DEV_URL = domain().toString();
